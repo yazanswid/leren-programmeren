5d557b3914771ecd176e20096168161689b41a6a
yazanswid <99081699@mydavinci.nl>
2024-12-04 13:58:16 +0100

    Create M&M's.py

---
+++ module 2/deel 2/M&M's.py
@@ -0,0 +1,20 @@
+import random
+
+
+kleuren = ("oranje", "blauw", "groen", "bruin")
+
+
+def vul_zak(aantal):
+    zak = []
+    for _ in range(aantal):
+        zak.append(random.choice(kleuren))
+    return zak
+
+aantal_mms = int(input("Hoeveel M&M's wil je aan de zak toevoegen? "))
+
+
+mm_zak = vul_zak(aantal_mms)
+
+
+print("De inhoud van de zak met M&M's is:")
+print(mm_zak)
