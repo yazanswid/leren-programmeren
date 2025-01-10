c492653a8fa9c7d662051bacd0ab483ad887b5db
yazanswid <99081699@mydavinci.nl>
2025-01-10 09:20:52 +0100

    Create stoplicht.py

---
+++ module 2/deel 3/stoplicht.py
@@ -0,0 +1,21 @@
+import time
+
+def stoplicht_simulatie():
+    while True:
+       
+        for _ in range(20):
+            print("Rood")
+            time.sleep(1)
+        
+       
+        for _ in range(30):
+            print("Groen")
+            time.sleep(1)
+        
+        # Oranje licht
+        for _ in range(10):
+            print("Oranje")
+            time.sleep(1)
+
+
+stoplicht_simulatie()
