b0d0a99869b41c7cc6add86693872f8b2fe134c5
yazanswid <99081699@mydavinci.nl>
2025-01-20 11:19:41 +0100

    Update basic2.py

--- module 2/prfm2/robotarm/basic2.py
+++ module 2/prfm2/robotarm/basic2.py
@@ -1,20 +1,26 @@
 from RobotArm import RobotArm
 
 # Import the challenges (in this case challenges/example.py)
-from challenges.example import challenges
+from challenges.basic import challenges
 
 # load the robotarm with a challenge on a level (max 3)
 robotArm = RobotArm(challenges[2],0)
 
 # your code starts here:
 robotArm.moveRight()
+robotArm.grab()
+robotArm.moveRight()
+robotArm.moveRight()
+robotArm.drop()
+robotArm.moveLeft()
+robotArm.grab()
+robotArm.moveLeft()
+robotArm.drop()
+robotArm.moveRight()
 robotArm.moveRight()
 robotArm.grab()
 robotArm.moveLeft()
-robotArm.moveLeft()
 robotArm.drop()
-
-
 # your code ends here
 
 # report the results of the mission
