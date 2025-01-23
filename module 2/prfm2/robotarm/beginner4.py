f6df791e8b961bfc48b934d9881d9eef26fb3363
yazanswid <99081699@mydavinci.nl>
2025-01-23 13:35:48 +0100

    Create beginner4.py

---
+++ module 2/prfm2/robotarm/beginner4.py
@@ -0,0 +1,35 @@
+from RobotArm import RobotArm
+
+# Import the challenges (in this case challenges/example.py)
+from challenges.beginner import challenges
+
+# load the robotarm with a challenge on a level (max 3)
+robotArm = RobotArm(challenges[4],0)
+
+# your code starts here:
+robotArm.moveRight()  
+
+for _ in range(6):  
+    robotArm.grab() 
+    color = robotArm.scan()  
+
+    if color == "white":  
+        robotArm.moveLeft()  
+        robotArm.drop()  
+        robotArm.moveRight()  
+    elif color == "red":  
+        robotArm.moveRight() 
+        robotArm.drop()  
+        robotArm.moveLeft()  
+    elif color is None:  
+        break  
+# report the results of the mission
+robotArm.report()
+
+# want help? Unlock code below!
+robotArm.help()
+
+# want to inspect a solution? Unlock code below!
+# robotArm.showSolution()
+# robotArm.wait()
+
