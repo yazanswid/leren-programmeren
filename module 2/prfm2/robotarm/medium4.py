b7e27845867c33f378b98f42f92fd3342ef2a591
yazanswid <99081699@mydavinci.nl>
2025-01-23 15:59:01 +0100

    Create medium4.py

---
+++ module 2/prfm2/robotarm/medium4.py
@@ -0,0 +1,35 @@
+from RobotArm import RobotArm
+
+# Import the challenges (in this case challenges/example.py)
+from challenges.medium import challenges
+
+# load the robotarm with a challenge on a level (max 3)
+robotArm = RobotArm(challenges[4],0)
+
+# your code starts here:
+for _ in range(5):
+    robotArm.grab()
+    robotArm.moveRight()
+    robotArm.moveRight()
+    robotArm.drop()
+    robotArm.moveLeft()
+    robotArm.moveLeft()
+
+robotArm.moveRight()
+robotArm.moveRight()
+for i in range(5):
+    robotArm.grab()
+    robotArm.moveLeft()
+    robotArm.drop()
+    robotArm.moveRight()
+
+# report the results of the mission
+robotArm.report()
+
+# want help? Unlock code below!
+robotArm.help()
+
+# want to inspect a solution? Unlock code below!
+# robotArm.showSolution()
+# robotArm.wait()
+
