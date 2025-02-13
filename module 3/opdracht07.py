49553c99d742907e25221105c811c4a9894d9c8d
yazanswid <99081699@mydavinci.nl>
2025-02-13 15:39:46 +0100

    Update opdracht07.py

--- module 3/opdracht07.py
+++ module 3/opdracht07.py
@@ -2,4 +2,4 @@ from fruitmand import fruitmand
 
 round_fruits = [fruit['name'] for fruit in fruitmand if fruit['round']]
 print("Ronde fruitsoorten:")
-print(", ".join(round_fruits))
+print((round_fruits))
