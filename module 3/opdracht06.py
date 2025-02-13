60991a49ad6415fc556767771db8ec3fdaafefa5
yazanswid <99081699@mydavinci.nl>
2025-02-13 12:59:35 +0100

    Update opdracht06.py

--- module 3/opdracht06.py
+++ module 3/opdracht06.py
@@ -1,7 +1,3 @@
 from fruitmand import fruitmand
 fruit_gewicht = [str(fruit['weight']) for fruit in fruitmand]
 print("g".join(fruit_gewicht))
-
-
-fruit_gewicht = [str(fruit['weight']) for fruit in fruitmand]
-print("g".join(fruit_gewicht))
