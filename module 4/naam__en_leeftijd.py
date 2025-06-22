4b3a56265bcf6e141113f6241a5708d99da26871
yazanswid <99081699@mydavinci.nl>
2025-06-22 17:18:00 +0200

    Update naam__en_leeftijd.py

--- module 4/naam__en_leeftijd.py
+++ module 4/naam__en_leeftijd.py
@@ -1,7 +1,21 @@
 def vraag_naam_en_leeftijd():
-    naam = input("Wat is je naam? ")
+    naam = input("Wat is je naam? ").strip().capitalize()
     leeftijd = input("Wat is je leeftijd? ")
+    while not leeftijd.isdigit():
+        print("Ongeldige invoer. Voer een geldige leeftijd in.")
+        leeftijd = input("Wat is je leeftijd? ")
     return {'naam': naam, 'leeftijd': leeftijd}
 
-gegevens = vraag_naam_en_leeftijd()
-print(f"{gegevens['naam']} is {gegevens['leeftijd']} jaar")
+def verzamel_gegevens():
+    gegevens_lijst = []
+    while True:
+        gegevens = vraag_naam_en_leeftijd()
+        gegevens_lijst.append(gegevens)
+        doorgaan = input("Toets enter om door te gaan of stop om te printen: ").strip()
+        if doorgaan.lower() == 'stop':
+            break
+    return gegevens_lijst
+
+alle_gegevens = verzamel_gegevens()
+for persoon in alle_gegevens:
+    print(f"{persoon['naam']} is {persoon['leeftijd']} jaar")
