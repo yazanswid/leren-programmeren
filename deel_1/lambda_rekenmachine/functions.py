1f1bda23cc95da8d39d43831181adcf79691b133
yazanswid <99081699@mydavinci.nl>
2026-05-11 11:54:36 +0200

    rekenmachine2

---
+++ deel_1/lambda_rekenmachine/functions.py
@@ -0,0 +1,13 @@
+def addition(number1, number2):
+    return number1 + number2
+
+def subtraction(number1, number2):
+    return number1 - number2
+
+def multiplication(number1, number2):
+    return number1 * number2
+
+def division(number1, number2):
+    if number2 == 0:
+        return "Kan niet delen door 0"
+    return number1 / number2
