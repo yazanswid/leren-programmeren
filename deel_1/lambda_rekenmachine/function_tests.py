b490101111865f9ba758102fb72f97bf1b80599a
yazanswid <99081699@mydavinci.nl>
2026-05-11 11:56:53 +0200

    Update function_tests.py

--- deel_1/lambda_rekenmachine/function_tests.py
+++ deel_1/lambda_rekenmachine/function_tests.py
@@ -1,3 +1,12 @@
 import test_lib
 import functions
 import calculations
+
+# tests voor de lambda rekenmachine functies
+
+test_lib.test('addition', 5, functions.addition(2, 3))
+test_lib.test('subtraction', 1, functions.subtraction(3, 2))
+test_lib.test('multiplication', 12, functions.multiplication(3, 4))
+test_lib.test('division', 2, functions.division(6, 3))
+
+test_lib.report()
